343e5eb4c1fe3061399d58e6bcea07e042d80f17
Joseph <z@charmed.tech>
2019-03-28 07:52:19 -0700

    Move product/category classes to api-server

--- api-server/src/models/products.js
+++ api-server/src/models/products.js
@@ -1,23 +1,50 @@
 'use strict';
 
-const uuid = require('uuid/v4');
-
-const schema = {};
-
+const Product = require('./products.schema.js');
+/**
+ * @param  {} id
+ * @param  {} {letrecord;if(id
+ * @param  {} {record=Product.findById(id
+ * @param  {} ;}else{record=Product.find(
+ * @param  {} ;}returnrecord;}post(entry
+ * @param  {} {entry.name=entry.name.toUpperCase(
+ * @param  {} ;constp=newProduct(entry
+ * @param  {} ;returnp.save(
+ * @param  {} ;}put(id
+ * @param  {} entry
+ * @param  {} {entry.name=entry.name.toUpperCase(
+ * @param  {} ;constresult=Product.findByIdAndUpdate(id
+ * @param  {} entry
+ * @param  {true}} {new
+ */
 class Products {
-  constructor() {
-    this.database = [];
+  get(id) {
+    let record;
+    if (id) {
+      record = Product.findById(id);
+    } else {
+      record = Product.find();
+    }
+    return record;
   }
 
-  get(id) {}
+  post(entry) {
+    entry.name = entry.name.toUpperCase();
+    const p = new Product(entry);
+    return p.save();
+  }
 
-  post(entry) {}
+  put(id, entry) {
+    entry.name = entry.name.toUpperCase();
+    const result = Product.findByIdAndUpdate(id, entry, { new: true });
 
-  put(id, entry) {}
+    return result;
+  }
 
-  delete(id) {}
-
-  sanitize(entry) {}
+  delete(id) {
+    const result = Product.findOneAndRemove({ id });
+    return result;
+  }
 }
 
 module.exports = Products;
